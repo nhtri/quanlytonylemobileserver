be52e95011be63e8823ad53731f4f2afdbc8fbab
tringuyen <tri.nh@hotmail.com>
2022-01-20 14:05:37 +0700

    tonylemobileserver

--- main.js
+++ main.js
@@ -514,79 +514,79 @@ app.post('/danhsachdonhangtemp/', function (req, res) {
     });
 });
 
-app.get('/getloaimay/', function (req, res) {
+app.get('/getdungluong/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select loaimay from loaimay', function (error, results, fields) {
+    pool.query('select dungluong from dungluong', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
 });
 
-app.get('/getdoimay/', function (req, res) {
+app.get('/getloaisanpham/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select doimay from doimay', function (error, results, fields) {
+    pool.query('select loaisanpham from loaisanpham', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
 });
 
-app.get('/getmanhinh/', function (req, res) {
+app.get('/getmau/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select manhinh from manhinh', function (error, results, fields) {
+    pool.query('select mau from mau', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
 });
 
-app.get('/getchip/', function (req, res) {
+app.get('/getnhomsanpham/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select chip from chip', function (error, results, fields) {
+    pool.query('select nhomsanpham from nhomsanpham', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
 });
 
-app.get('/getram/', function (req, res) {
+app.get('/getphienban/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select ram from ram', function (error, results, fields) {
+    pool.query('select phienban from phienban', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
 });
 
-app.get('/getocung/', function (req, res) {
+app.get('/gettensanpham/', function (req, res) {
     
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
     console.log(req);
-    pool.query('select ocung from ocung', function (error, results, fields) {
+    pool.query('select tensanpham from tensanpham', function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
